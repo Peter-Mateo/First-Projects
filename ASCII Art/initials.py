7e9b5800c945f050200998d7a1d0b8308d18f350
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-05-31 07:56:17 -0400

    Simple Design

---
+++ ASCII Art/initials.py
@@ -0,0 +1,12 @@
+#Peter Mateo
+#I love to learn and improve
+
+print("  PPP   M     ")
+print(" P   P  M     ")
+print(" P      M     ")
+print("  PPP   M     ")
+print("     P  M     ")
+print(" P   P  M     ")
+print("  PPP   MMMMM ")
+
+
