1015ac33b893f77c02bea71200d9c5443774176b
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-05-31 08:04:43 -0400

    Added Initials

--- ASCII Art/initials.py
+++ ASCII Art/initials.py
@@ -10,3 +10,18 @@ print(" P   P  M     ")
 print("  PPP   MMMMM ")
 
 
+print("   PPPPPPP     ")
+print("   PP     PP   ")
+print("   PP     PP   ")
+print("   PPPPPPP     ")
+print("   PP          ")
+print("   PP          ")
+print("   PP          ")
+
+print("PPP         PPP")
+print("PPPP       PPPP")
+print("PP PP    PP  PP")
+print("PP  PPPPPP   PP")
+print("PP           PP")
+print("PP           PP")
+print("PP           PP")
