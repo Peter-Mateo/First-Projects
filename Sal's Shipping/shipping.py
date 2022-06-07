aa33bb2dd682ae670bddfb96213227c4647d44a0
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-06-07 04:38:18 -0400

    Drone Shipping

--- Sal's Shipping/shipping.py
+++ Sal's Shipping/shipping.py
@@ -29,3 +29,23 @@ else:
 groundshipping_premium = 125.00
 print("To ship with Ground shipping Premium it will cost")
 print(groundshipping_premium)
+
+#Drone Shipping
+if weight <= 2:
+    price = (weight * 4.50) + droneship_flat
+    print("To ship your package with Drone shipping it will cost:")
+    print(price)
+
+elif weight > 2 and weight <= 6:
+    price = (weight * 9.00) + droneship_flat
+    print("To ship your package with Drone shipping it will cost:")
+    print(price)
+
+elif weight > 6 and weight <= 10:
+    price = (weight * 12.00) + droneship_flat
+    print("To ship your package with Drone shipping it will cost:")
+    print(price)
+else:
+    price = (weight * 14.25) + droneship_flat
+    print("To ship your package with Drone shipping it will cost:")
+    print(price)
