42a24b656d536b0b58636e83a7e59e0e74e81ca7
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-06-07 04:35:48 -0400

    Declaring Variables

--- Sal's Shipping/shipping.py
+++ Sal's Shipping/shipping.py
@@ -1,9 +1,3 @@
-weight = 6
-
-#Ground Shipping 
-if weight < 2:
-    if weight < 1:
-        print("It will cost $21.50")
-    else:
-        print("It will cost $23.50")
-
+weight = 8.4
+groundshipping_flat = 20
+droneship_flat = 0
