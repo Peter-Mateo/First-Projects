e9a6fd61e6a8133965d05a5fbfd99d943fcd826f
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-06-07 04:37:01 -0400

    Ground Shipping

--- Sal's Shipping/shipping.py
+++ Sal's Shipping/shipping.py
@@ -1,3 +1,26 @@
+
+
 weight = 8.4
 groundshipping_flat = 20
 droneship_flat = 0
+
+#Ground Shipping
+if weight <= 2:
+    price = (weight * 1.50) + groundshipping_flat 
+    print("To ship your package with ground shipping it will cost:")
+    print(price)
+
+elif weight > 2 and weight <= 6:
+    price = (weight * 3.00) + groundshipping_flat 
+    print("To ship your package with ground shipping it will cost:")
+    print(price)
+
+elif weight > 6 and weight <= 10:
+    price = (weight * 4.00) + groundshipping_flat 
+    print("To ship your package with ground shipping it will cost:")
+    print(price)
+
+else:
+    price = (weight * 4.75) + groundshipping_flat 
+    print("To ship your package with ground shipping it will cost:")
+    print(price)
