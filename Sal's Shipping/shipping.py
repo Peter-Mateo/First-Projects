f6202a00c3a997d3d5d8c12e8a1a09b98b1e2243
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-06-07 04:43:20 -0400

    Update shipping.py
    Added Rounding - round to two decimal places

--- Sal's Shipping/shipping.py
+++ Sal's Shipping/shipping.py
@@ -1,27 +1,27 @@
 
 
-weight = 8.4
+weight = 4.8
 groundshipping_flat = 20
 droneship_flat = 0
 
 #Ground Shipping
 if weight <= 2:
-    price = (weight * 1.50) + groundshipping_flat 
+    price = str(round((weight * 1.50) + groundshipping_flat, 2)) 
     print("To ship your package with ground shipping it will cost:")
     print(price)
 
 elif weight > 2 and weight <= 6:
-    price = (weight * 3.00) + groundshipping_flat 
+    price = str(round((weight * 3.00) + groundshipping_flat,2)) 
     print("To ship your package with ground shipping it will cost:")
     print(price)
 
 elif weight > 6 and weight <= 10:
-    price = (weight * 4.00) + groundshipping_flat 
+    price = str(round((weight * 4.00) + groundshipping_flat, 2)) 
     print("To ship your package with ground shipping it will cost:")
     print(price)
 
 else:
-    price = (weight * 4.75) + groundshipping_flat 
+    price = str(round((weight * 4.75) + groundshipping_flat, 2)) 
     print("To ship your package with ground shipping it will cost:")
     print(price)
 
@@ -32,20 +32,20 @@ print(groundshipping_premium)
 
 #Drone Shipping
 if weight <= 2:
-    price = (weight * 4.50) + droneship_flat
+    price = str(round((weight * 4.50) + droneship_flat, 2))
     print("To ship your package with Drone shipping it will cost:")
     print(price)
 
 elif weight > 2 and weight <= 6:
-    price = (weight * 9.00) + droneship_flat
+    price = str(round((weight * 9.00) + droneship_flat,2))
     print("To ship your package with Drone shipping it will cost:")
     print(price)
 
 elif weight > 6 and weight <= 10:
-    price = (weight * 12.00) + droneship_flat
+    price = str(round((weight * 12.00) + droneship_flat, 2))
     print("To ship your package with Drone shipping it will cost:")
     print(price)
 else:
-    price = (weight * 14.25) + droneship_flat
+    price = str(round((weight * 14.25) + droneship_flat, 2))
     print("To ship your package with Drone shipping it will cost:")
     print(price)
