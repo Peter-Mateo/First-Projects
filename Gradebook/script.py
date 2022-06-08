0610155d56beba8f2bd38065eeeb1f5d770690ef
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-06-08 13:29:11 -0400

    Update script.py
    Removed " which created problems of a non closing tag

--- Gradebook/script.py
+++ Gradebook/script.py
@@ -3,3 +3,8 @@ last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["arc
 subjects = ["physics", "calculus", "poetry", "history"]
 grades = [98, 97, 85, 88]
 
+#Two dimensional List
+gradebook = [["physics", 98], ["calculus", 97], ["poetry", 98], ["history", 88]] 
+
+#Test 
+print(gradebook)
