36d54edcc8d132f249af84bac9d1cdd59380faa0
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-06-11 23:29:32 -0400

    Update For_Loop_Basic.py

--- Coding_Dojo_Python/Fundamentals/fundamentals/For_Loop_Basic.py
+++ Coding_Dojo_Python/Fundamentals/fundamentals/For_Loop_Basic.py
@@ -19,4 +19,11 @@ for i in range(1):
     x = 0 
     for i in range(100):
         x = x + 1
-        if x == ()
+        if x == 0:
+            print("0")
+        elif x%10 == 0:
+            print("Coding Dojo")
+        elif x%5 == 0:
+            print("Coding")
+        else:
+            print(x)
