d660165d603210259814600eba49ba2271f75e24
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-06-11 23:16:47 -0400

    Create For_Loop_Basic.py

---
+++ Coding_Dojo_Python/Fundamentals/fundamentals/For_Loop_Basic.py
@@ -0,0 +1,22 @@
+
+for i in range(1):
+    x = 0
+    for i in range(150):
+        x = x + 1
+        print(x)
+
+print("--------------------------")
+
+for i in range(1):
+    a = 0
+    for i in range(200):
+        a = a + 5
+        print(a)
+
+print("--------------------------")
+
+for i in range(1):
+    x = 0 
+    for i in range(100):
+        x = x + 1
+        if x == ()
