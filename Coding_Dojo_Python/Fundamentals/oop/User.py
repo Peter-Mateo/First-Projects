2d1b39d3322091ae5a23d5424b420bcb25b83988
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-06-30 06:59:08 -0400

    Update User.py

---
+++ Coding_Dojo_Python/Fundamentals/oop/User.py
@@ -0,0 +1,11 @@
+
+
+class user:
+    def __init__(self, first, last, email, age):
+        self.first_name = first
+        self.last_name = last
+        self.email = email
+        self.age = age
+        self.is_rewards_member = False
+        self.gold_card_points = 0
+
