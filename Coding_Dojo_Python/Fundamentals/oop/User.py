540779b43b7117f9aef316b71aa92ea24579e158
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-07-03 05:27:32 -0400

    Chaining Methods Practice

--- Coding_Dojo_Python/Fundamentals/oop/User.py
+++ Coding_Dojo_Python/Fundamentals/oop/User.py
@@ -8,6 +8,7 @@ class user:
         self.age = age
         self.is_rewards_member = False
         self.gold_card_points = 0
+        
 
     def display_info(self):
         print(self.first_name)
@@ -16,11 +17,13 @@ class user:
         print(self.age)
         print(self.is_rewards_member)
         print(self.gold_card_points)
+        return self
 
     def enroll(self):
         if self.is_rewards_member != True:
             self.is_rewards_member = True
             self.gold_card_points = 200
+            return self
         else:
             print("User already a member.")
             return False
@@ -28,30 +31,32 @@ class user:
     def spend_points(self, amount):
         if self.gold_card_points > amount:
             print("You don't have enough points to spend")
+            return self
         else:
             self.gold_card_points -= amount
+            return self
 
 
 # Testing display_info
 user_peter = user("Peter", "Mateo", "p305127@icloud.com", 21)
 
-user_peter.display_info()
+user_peter.display_info().enroll().enroll()
 
-# Testing Enroll
-user_peter.enroll()
-print("--------------------------")
-user_peter.enroll()
+# Testing Enroll (- Practicing Chaining Methods - )
+#user_peter.enroll()
+#print("--------------------------")
+#user_peter.enroll()
 
 
 #2 More Instances of User class
 user_leo = user("Leo", "Mateo", "leom305131@gmail.com", 21)
 user_steven = user("Steven", "Mateo", "stevenmateooo@gmail.com", 24)
 
-#User Spending
+#User Spending (- Practicing Chaining Methods - )
 user_leo.spend_points(50)
 print("--------------------------")
-user_steven.enroll()
-user_steven.spend_points(80)
+user_steven.enroll().spend_points(80)
+#user_steven.spend_points(80)
 print("--------------------------")
 
 #Calling the DisplayInfo 
