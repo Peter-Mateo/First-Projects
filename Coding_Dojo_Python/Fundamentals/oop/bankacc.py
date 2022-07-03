3c0c4aef799b09fe5da3ed3a337e8e507ac21229
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-07-03 05:57:20 -0400

    Edits

--- Coding_Dojo_Python/Fundamentals/oop/bankacc.py
+++ Coding_Dojo_Python/Fundamentals/oop/bankacc.py
@@ -1,41 +1,40 @@
 class BankAccount:
     accounts = []
-    def init(self,int_rate,balance):
+    # don't forget to add some default values for these parameters!
+    def __init__(self, int_rate, balance): 
+        # your code here! (remember, instance attributes go here)
         self.int_rate = int_rate
         self.balance = balance
-        BankAccount.accounts.append(self)
-
+        # don't worry about user info here; we'll involve the User class soon
     def deposit(self, amount):
+        # your code here
         self.balance += amount
         return self
 
-    def withdraw(self,amount):
-        if(self.balance - amount) >= 0:
-            self.balance -= amount
-        else:
-            print("Insufficient Funds: Charging a $5 fee")
-            self.balance -= 5
+    def withdraw(self, amount):
+        # your code here
+        self.balance -= amount
         return self
 
     def display_account_info(self):
-        print(f"Balance: {self.balance}")
+        # your code here
+        print(self.balance)
+        return
+    def yield_interest(self):
+        # your code here
+        print(self.int_rate)
         return self
-
-    def yeild_interest(self):
-        if self.balance > 0:
-            self.balance += (self.balance * self.int_rate)
-        return self
-
     @classmethod
     def print_all_accounts(cls):
         for account in cls.accounts:
             account.display_account_info()
 
+BankAccount_peter = BankAccount(4,500)
+BankAccount_leo = BankAccount(3, 5000)
 
-savings = BankAccount(.05,1000)
-checking = BankAccount(.02,5000)
+BankAccount_peter.deposit(10).deposit(50).deposit(20).withdraw(50).yield_interest().display_account_info()
+BankAccount_leo.deposit(32).deposit(32).withdraw(40).withdraw(2000).withdraw(30).withdraw(320).yield_interest().display_account_info()
 
-savings.deposit(10).deposit(20).deposit(40).withdraw(600).yeild_interest().display_account_info()
-checking.deposit(100).deposit(200).deposit(400).withdraw(60).yeild_interest().display_account_info()
+
 
 BankAccount.print_all_accounts()
