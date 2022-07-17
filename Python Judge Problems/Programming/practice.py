855220709b82bcc1057758e8db953bea39cf7d38
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-07-17 13:21:17 -0400

    Update practice.py

--- Python Judge Problems/Programming/practice.py
+++ Python Judge Problems/Programming/practice.py
@@ -1,8 +1,13 @@
 class Robot:
 
+    def __init__(self, name, color, weight):
+        self.name = name
+        self.color = color
+        self.weight = weight
+        
     def introduce_self(self):
         print("My name is " + self.name)
-
+"""
 r1 = Robot()
 r1.name = "Tom"
 r1.color = "red"
@@ -14,6 +19,9 @@ r2 = Robot()
 r2.name = "Jerry"
 r2.color = "blue"
 r2.weight = 40
+"""
 
+r1 = Robot("Peter", "brown", 175)
+r2 = Robot("Jerry", "blue", 40)
 r1.introduce_self()
 r2.introduce_self()
