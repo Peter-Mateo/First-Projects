d38ebe9b83ddfcbd0fa576ef664cc305ff0de8e1
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-07-17 13:16:09 -0400

    Update practice.py

--- Python Judge Problems/Programming/practice.py
+++ Python Judge Problems/Programming/practice.py
@@ -1,43 +1,19 @@
-import random as rnd
-import math as mth
-import datetime as dt
+class Robot:
+
+    def introduce_self(self):
+        print("My name is " + self.name)
+
+r1 = Robot()
+r1.name = "Tom"
+r1.color = "red"
+r1.weight = 30
 
 
-answer = rnd.randint(1,4)
 
-if answer == 1:
-    print("You are happy!!")
+r2 = Robot()
+r2.name = "Jerry"
+r2.color = "blue"
+r2.weight = 40
 
-elif answer == 2:
-    print("You are enough!")
-
-elif answer == 3:
-    print("She wasn't worth it!")
-
-elif answer == 4:
-    print("You did it!!")
-
-quantity = 3
-item_price = 4000.15
-sales_tax = 0.065
-
-print(f"Quantity: {quantity}")
-print(f"item_price: ${item_price}")
-print(f"Sales Tax: {sales_tax :.1%}")
-print(f"Total: ${quantity * item_price * sales_tax :,.2f}")
-
-print("-" * 15)
-z = complex(2, -3)
-
-print(z)
-
-birth_date = dt.date(2001, 6, 27)
-now = dt.date.today()
-
-age = now - birth_date
-delta_age = age.days
-age_years = delta_age // 365
-months = (delta_age % 365) // 30
-
-print(age)
-print(age_years , months)
+r1.introduce_self()
+r2.introduce_self()
