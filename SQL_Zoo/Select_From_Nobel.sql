9dd77d3d60fd96d20f2134622127e406bec95359
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-07-24 21:33:08 -0400

    Question 2

--- SQL_Zoo/Select_From_Nobel.sql
+++ SQL_Zoo/Select_From_Nobel.sql
@@ -1,6 +1,12 @@
---Winners From 1950
+-- SELECT from Nobel Tutorial
 
 --Change the query shown so that it displays Nobel prizes for 1950.
 SELECT yr, subject, winner
 FROM nobel
 WHERE yr = 1950
+
+--Show who won the 1962 prize for literature.
+SELECT winner
+FROM nobel
+WHERE yr = 1962
+AND subject = 'literature'
