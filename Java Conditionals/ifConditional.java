8e0cf69de4d655b7856da51861d2f44833a98096
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-09-25 18:04:49 -0400

    practice

--- Java Conditionals/ifConditional.java
+++ Java Conditionals/ifConditional.java
@@ -1,8 +1,27 @@
 public class ifConditional{
     public static void main(String[] args){
         int number5 = 5;
-        if (number5 == 5){
-            System.out.println("Is equal to number 5");
+        switch(number5){
+            case 1:
+                System.out.println("Yaga");
+                break;
+            case 2:
+                System.out.println("Baby Yaga");
+                break;
+            case 3:
+                System.out.println("Yaga Yaga");
+                break;
+            case 4:
+                System.out.println("yaga");
+                break;
+            case 5:
+                System.out.println("ooooh");
+                break;
+            case 6:
+                System.out.println("this ain't it chief");
+                break;
+            default:
+                System.out.println("Default Baby");
+        }
         }
     }
-}
