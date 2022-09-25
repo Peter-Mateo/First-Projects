f21d71b42a85043eff096f16ec98e9c6a198f096
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-09-25 17:44:30 -0400

    practice

---
+++ Java Conditionals/ifConditional.java
@@ -0,0 +1,8 @@
+public class ifConditional{
+    public static void main(String[] args){
+        int number5 = 5;
+        if (number5 == 5){
+            System.out.println("Is equal to number 5");
+        }
+    }
+}
