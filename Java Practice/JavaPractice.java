79226ec62584d9cd71a4b766d760e1f824fe6c17
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-10-02 15:01:41 -0400

    Practice

--- Java Practice/JavaPractice.java
+++ Java Practice/JavaPractice.java
@@ -1,6 +1,20 @@
+import java.util.ArrayList;
 public class JavaPractice {
     public static void main(String [] args){
-        String[] list2 = {"Peter", "Mateo", "Luis"};
-        System.out.println(list2.length);
+        // Creating the Array List 
+        ArrayList<Double> grades = new ArrayList<Double>();
+        // Adds values to the Array List
+        grades.add(3.5);
+        grades.add(2.6);
+        // Creating a List of the names
+        String[] names = {"Peter", "Leonardo"};
+        // Array List combining the students gpa and names
+        ArrayList<Object> gradesAndNames = new ArrayList<Object>();
+        // For loop to combine the values
+        for (int i = 0; i < grades.size(); i++ ){
+            gradesAndNames.add(grades.get(i) + " " + names[i]);
+        }
+        // Printing the list
+        System.out.println(gradesAndNames);
     }
 }
