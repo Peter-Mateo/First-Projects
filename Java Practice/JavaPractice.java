a3db20aed5ea774a06cd4de28c6c7a892077b152
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-10-02 15:08:36 -0400

    Practice

--- Java Practice/JavaPractice.java
+++ Java Practice/JavaPractice.java
@@ -1,13 +1,9 @@
 import java.util.ArrayList;
 public class JavaPractice {
     public static void main(String [] args){
-        // Creates the Array List
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
         // Creating for loop for numbers 1-100
         for (int i = 0; i <= 100; i++){
-            numbers.add(i);
+            System.out.println(i);
         }
-        // Printing out the list
-        System.out.println(numbers);
     }
 }
